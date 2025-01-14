569dfced5ea2ccf281481188b2e1fb6a312f48e7
Shreyas Damase <113159691+ShreyasDamase@users.noreply.github.com>
2025-01-14 10:17:00 +0530

    Update firebaseConfig.js

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -7,12 +7,12 @@ import {
 import AsyncStorage from "@react-native-async-storage/async-storage"; // Import AsyncStorage
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCW-O6WaXX2ck4kw5hBEWvEj97MhrdOGE0",
-  authDomain: "netflix-project-45140.firebaseapp.com",
-  projectId: "netflix-project-45140",
-  storageBucket: "netflix-project-45140.firebasestorage.app",
-  messagingSenderId: "393846544231",
-  appId: "1:393846544231:web:88c56ca98209e108ebc1c2",
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
 };
 
 // Initialize Firebase App
